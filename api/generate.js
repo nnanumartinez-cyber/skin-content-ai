461b2b25f8b2e09610b226322ebbfa6016d0c5db
nnanumartinez-cyber <nnanu.martinez@icloud.com>
2026-05-21 23:21:38 -0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -22,8 +22,8 @@ module.exports = async function handler(req, res) {
       });
     }
 
-    const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+   const response = await fetch(
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apiKey}`,
       {
         method: "POST",
         headers: {
